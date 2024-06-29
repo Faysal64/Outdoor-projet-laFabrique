df68bcdf7d6f6ed05d5969ef82b70e4e8eb286c7
Faysal Driouach <fdriouach10@gmail.com>
2024-06-29 16:48:55 +0200

    commit 29/06/24

--- script.js
+++ script.js
@@ -1,23 +1,27 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const slides = document.querySelector('.slides');
-    const navDots = document.querySelectorAll('.nav-dot');
+    const carousels = document.querySelectorAll('.carousel');
 
-    let currentIndex = 0;
+    carousels.forEach((carousel) => {
+        const slides = carousel.querySelector('.slides');
+        const navDots = carousel.querySelectorAll('.nav-dot');
 
-    navDots.forEach((dot, index) => {
-        dot.addEventListener('click', () => {
-            currentIndex = index;
-            updateCarousel();
+        let currentIndex = 0;
+
+        navDots.forEach((dot, index) => {
+            dot.addEventListener('click', () => {
+                currentIndex = index;
+                updateCarousel();
+            });
         });
+
+        function updateCarousel() {
+            const slideWidth = slides.children[0].offsetWidth + 30; // Inclut le gap de 30px
+            slides.style.transform = `translateX(${-currentIndex * slideWidth}px)`;
+
+            navDots.forEach(dot => dot.classList.remove('active'));
+            navDots[currentIndex].classList.add('active');
+        }
+
+        updateCarousel();
     });
-
-    function updateCarousel() {
-        const slideWidth = slides.children[0].offsetWidth + 30; // Inclut le gap de 30px
-        slides.style.transform = `translateX(${-currentIndex * slideWidth}px)`;
-
-        navDots.forEach(dot => dot.classList.remove('active'));
-        navDots[currentIndex % 4].classList.add('active');
-    }
-
-    updateCarousel();
 });
