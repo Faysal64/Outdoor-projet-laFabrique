af0012b53cbd509606a49700851a5ce462a07277
Faysal Driouach <fdriouach10@gmail.com>
2024-07-02 23:49:06 +0200

    commit 02/06/2024

--- script.js
+++ script.js
@@ -27,11 +27,3 @@ document.addEventListener('DOMContentLoaded', function () {
 });
 
 
-document.getElementById('menuButton').addEventListener('click', function () {
-    var navbar = document.getElementById('navbar');
-    if (navbar.style.display === 'none' || navbar.style.display === '') {
-        navbar.style.display = 'block';
-    } else {
-        navbar.style.display = 'none';
-    }
-});
